5de7bdfccbb87a2b8cde7ef1b4a00ac3cc22810f
yunxiwangluo <liangming@52yunxi.cn>
2020-05-14 16:19:17 +0800

    添加iOS端乐播云sdk所需的方法

--- lib/lblelinkplugin.dart
+++ lib/lblelinkplugin.dart
@@ -6,6 +6,14 @@ class Lblelinkplugin {
   static const MethodChannel _channel =
       const MethodChannel('lblelinkplugin');
 
+  //初始化sdk
+  static initLBSdk(String appkey){
+
+    _channel.invokeMethod("initLBSdk",{"appKey":appkey});
+
+  }
+
+
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
